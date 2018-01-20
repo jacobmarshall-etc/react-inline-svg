0777a774a28ccc056a3fe6ed4abba72205d15e05
Jacob Marshall <git@jacobmarshall.co>
2018-01-20 22:39:59 +1300

    Remove need for span wrapper

--- src/react-inline-svg.js
+++ src/react-inline-svg.js
@@ -2,11 +2,35 @@ function request(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.addEventListener('readystatechange', function () {
     if (xhr.readyState === 4) {
-      callback(xhr.status === 200, xhr.responseText);
+      callback(xhr.status === 200, xhr.responseXML);
     }
   }, false);
   xhr.open('GET', url, true);
+  xhr.responseType = 'document';
   xhr.send();
+}
+
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+function excludeProps(originalProps, keys) {
+  var props = {};
+  for (var key in originalProps) {
+    if (hasOwnProperty.call(originalProps, key) && keys.indexOf(key) === -1) {
+      props[key] = originalProps[key];
+    }
+  }
+  return props;
+}
+
+function nodeToElement(svg) {
+  var attributes = svg.attributes;
+  var props = {
+    dangerouslySetInnerHTML: {__html: svg.innerHTML},
+  };
+  for (var i = 0; i < attributes.length; i++) {
+    var attribute = attributes[i];
+    props[attribute.name] = attribute.value;
+  }
+  return React.createElement('svg', props);
 }
 
 function extend(Child, Parent) {
@@ -21,7 +45,7 @@ function InlineSVG(props) {
   React.Component.call(this, props);
   this.state = {
     loading: true,
-    content: null,
+    element: null,
   };
 }
 
@@ -43,18 +67,17 @@ InlineSVG.prototype.load = function(src) {
   this.cancel = InlineSVG.cache.load(src, function(item) {
     that.setState({
       loading: false,
-      content: item
+      element: item
     });
   });
 };
 
 InlineSVG.prototype.render = function() {
-  return React.createElement('span', {
-    className: 'InlineSVG' +
-      (this.state.loading ? ' InlineSVG--loading' : '') +
-      (this.props.className ? ' ' + this.props.className : ''),
-    dangerouslySetInnerHTML: {__html: this.state.content}
-  });
+  return (
+    this.state.element
+      ? React.cloneElement(this.state.element, excludeProps(this.props, ['src']))
+      : null
+  );
 };
 
 var cache = InlineSVG.cache = {};
@@ -65,24 +88,24 @@ InlineSVG.cache.load = function(url, callback) {
     item = cache[url] = {
       url: url,
       loading: true,
-      content: null,
+      element: null,
       callbacks: [],
     };
     if (callback) {
       item.callbacks.push(callback);
     }
     request(url, function(success, content) {
-      item.content = content;
+      item.element = nodeToElement(content.documentElement);
       item.loading = false;
       while (item.callbacks.length) {
-        item.callbacks.shift()(content);
+        item.callbacks.shift()(item.element);
       }
     });
   } else if (callback) {
     if (item.loading) {
       item.callbacks.push(callback);
     } else {
-      callback(item.content);
+      callback(item.element);
     }
   }
   var cancelled = false;
